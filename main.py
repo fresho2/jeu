b57e5fd9811ff05a18be539bb6b65cd1e6e7eff1
Seho TOGBOSSI <setogbossi@math-2002-06l.sciences.lan>
2023-01-23 12:22:48 +0100

    ajout des fichier .txt pour tester et implementation de la fonction  importer_mots(nom_fichier)

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 #ceci est mon fichier principal
 import copy
 import random
+
+
+
+
+def importer_mots(nom_fichier):
+    """cette fonction prend en argument un nom de fichier
+        et renvoie une liste de mots contenus dans le fichier tout
+        en se limitant à trois (03) lettres minimum"""
+    fichier=open(nom_fichier)
+    liste=[]
+    for ligne in fichier:
+        l=ligne.strip()#supprime les caractère comme les retours à la ligne, les espaces et divers
+        if len(l)>=3:
+            liste.append(l.upper())#ajoute le mot en majuscule
+    return liste
+    fichier.close()
+
+
+
+
+
+
+if __name__=="__main__":
+    #programme principal
+    print(importer_mots("mots2.txt"))
+    
