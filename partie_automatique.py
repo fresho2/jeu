11f04e6a7010421c8e54a475e1db23d95a86a251
Ali ATES <alates@vdilinux18.sciences.lan>
2023-01-30 17:36:10 +0100

    ajout de la fonction fabrique_liste_alea

--- partie_automatique.py
+++ partie_automatique.py
@@ -1,7 +1,15 @@
 from mecanisme import *
-
+import random 
 def fabrique_liste_alphabet() :
     """la fonction n'a pas d'argument et renvoie une liste de lettre majuscule dans l'ordre alphabetique"""
     l=[chr(i+65) for i in range(26)] #creation d'une liste de charactere en comprehension
     return l
-print(fabrique_liste_alphabet())
+
+def fabrique_liste_alea():
+    """la fonction n'a pas d'argument et renvoie une liste d'alphabet en majuscule ordonnée de maniere aleatoire"""
+    a=list(fabrique_liste_alphabet())
+    random.shuffle(a)
+    return a
+
+print(fabrique_liste_alea())
+    
