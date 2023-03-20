fa01a532e4a19211af9719e8e3fe85529f0578e2
Seho TOGBOSSI <setogbossi@vdilinux01.sciences.lan>
2023-03-20 12:23:24 +0100

    supression

--- interface.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from partie_automatique import * 
-from partie_humaine import *
-
-def interface_graph():
-    """ cette foncion met en place l'interface graphique et ne renvoie rien """
-    root = Tk()
